c59610aaac12e11274a200b8b31f76ccaf199781
Yofri05 <jeffersitorila@gmail.com>
2024-05-29 10:06:23 -0600

    habilidades

--- Portafolio/obj/Debug/net6.0/Portafolio.AssemblyInfo.cs
+++ Portafolio/obj/Debug/net6.0/Portafolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portafolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72d9e0ca03b45f7b94ea7c713b850e24878c4ffa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d90f9af5ee9feb64a20557a93a3e531d4b70417c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portafolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portafolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

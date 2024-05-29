72d9e0ca03b45f7b94ea7c713b850e24878c4ffa
Yofri05 <jeffersitorila@gmail.com>
2024-05-29 08:46:12 -0600

    razor

--- Portafolio/obj/Debug/net6.0/Portafolio.AssemblyInfo.cs
+++ Portafolio/obj/Debug/net6.0/Portafolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portafolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b22df86e6a4d8fa37ed32b8c3bacc84857c3d1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cfb971cd28e7faab6e229e5e91b9606ee6a0c62")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portafolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portafolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

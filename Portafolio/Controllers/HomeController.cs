875ede95fe1d4b6da71f2b4b93508c7e2a233776
yofri05 <jeffersitorila@gmail.com.com>
2024-05-31 22:48:31 -0600

    Proveedores configuracion

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -11,26 +11,28 @@ namespace Portafolio.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IRepositorioProyectos repositorioProyectos;
+        private readonly IConfiguration configuration;
 
         public HomeController(ILogger<HomeController> logger,
-            IRepositorioProyectos repositorioProyectos
+            IRepositorioProyectos repositorioProyectos,
+            IConfiguration configuration
 
             )
         {
             _logger = logger;
             this.repositorioProyectos = repositorioProyectos;
-           
+            this.configuration = configuration;
         }
 
         public IActionResult Index()
         {
-
+            var apellido = configuration.GetValue<String>("Apellido");
             _logger.LogTrace("este es un mensaje de Trace");
             _logger.LogDebug("este es un mensaje de Debug");
             _logger.LogWarning("este es un mensaje de Warning");
             _logger.LogInformation("este es un mensaje de Information");
             _logger.LogError("este es un mensaje de Error");
-            _logger.LogCritical("este es un mensaje de critical");
+            _logger.LogCritical("este es un mensaje de critical"+apellido);
             var proyectos = repositorioProyectos.obtenerproyectos().Take(3).ToList();
             
 
